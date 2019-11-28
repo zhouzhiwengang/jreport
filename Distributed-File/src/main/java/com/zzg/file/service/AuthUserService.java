24f5a956f877c13a629b5e6c3e9f9246f44f4957
zhouzhiwengang <zhouzhiwengang@163.com>
2019-11-28 18:56:36 +0800

    "分布式文件功能封装"

---
+++ Distributed-File/src/main/java/com/zzg/file/service/AuthUserService.java
@@ -0,0 +1,15 @@
+package com.zzg.file.service;
+
+import java.util.List;
+import java.util.Map;
+import com.zzg.file.domain.AuthUser;
+import com.zzg.jreport.common.BaseService;
+import com.zzg.jreport.common.page.PageData;
+import com.zzg.jreport.common.page.PageParam;
+
+public interface AuthUserService extends BaseService<AuthUser>{
+	 // 方法梳理
+    List<AuthUser> selectAll(Map<String, Object> paramter);
+    
+    PageData<AuthUser> selectAllPage(Map<String,Object> parame, PageParam rb);
+}
